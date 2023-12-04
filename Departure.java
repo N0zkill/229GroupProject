239b40a791d2926082eb675419fbdab2ae797fe5
Mohammad Hassan <150965197+Mohammad-Y-Hassan@users.noreply.github.com>
2023-12-04 14:19:16 -0500

    Update Departure.java

--- Departure.java
+++ Departure.java
@@ -10,26 +10,44 @@ package Question_8;
  */
 import java.util.*;
 public class Departure {
+    
+    //List to store avalible runways
     private List<Runway> runways;
 
+    //Initialize the departure system    
     public Departure(int numRunways) {
+
+        //Initialize the list of runways
         this.runways = new ArrayList<>();
+
+        //Add objects to the list
         for (int i = 0; i < numRunways; i++) {
             runways.add(new Runway());
         }
     }
 
+    //Method that assigns plain to runway
     public void assignPlaneToRunway(Plane plane) {
+
+        //Check if there are available runways
         if (!runways.isEmpty()) {
             Random random = new Random();
+
+            //Selects a random runway and adds the plane to it
             Runway selectedRunway = runways.get(random.nextInt(runways.size()));
             selectedRunway.addPlane(plane);
-        } else {
+        } 
+        
+        //If no runways are avalible, this will display a message
+        else {
             System.out.println("No runways available for plane assignment.");
         }
     }
 
+    //Method to retrieve runway
     public List<Runway> getRunways() {
+
+        //Returns the runway list
         return runways;
     }
 }
